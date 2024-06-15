3daf9c74c11fb1a5c352beb26d0f8fbe4a323408
sumitkumar-SG <sumit.kumar@studiographene.com>
2024-06-15 19:57:36 +0530

    Exercise_Six

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,12 +1,40 @@
 import { restaurantList } from "./Constant.js";
+import { useEffect, useState } from "react";
 import { RestaurantCard as Card } from "./RestaurantCard.jsx";
 function Body() {
+  const [search, setSearch] = useState("");
+  const [filteredRestaurantList, setFilteredRestaurantList] = useState(restaurantList);
+  function filterRestaurantList(search, restaurants) {
+    return restaurants.filter((res) => res.data.name.toLowerCase().includes(search.toLowerCase()));
+  }
+  useEffect(() => {
+    if (search === "") {
+      setFilteredRestaurantList(restaurantList);
+    }
+  }, [filteredRestaurantList, search]);
   return (
-    <div className="body">
-      {restaurantList.map((res) => {
-        return <Card key={res.data.id} resData={res} />;
-      })}
-    </div>
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search for restaurants"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            setFilteredRestaurantList(filterRestaurantList(search, filteredRestaurantList));
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="body">
+        {filteredRestaurantList.map((res) => {
+          return <Card key={res.data.id} resData={res} />;
+        })}
+      </div>
+    </>
   );
 }
 
